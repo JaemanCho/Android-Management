740e4471db9529c0100c6fbe789244f89b57ac8a
Jaeman Cho <57745706+JaemanCho@users.noreply.github.com>
2019-12-16 15:07:19 +0900

    Add Login, Register, MainActivity

--- app/src/main/java/com/example/management/RegisterActivity.java
+++ app/src/main/java/com/example/management/RegisterActivity.java
@@ -3,18 +3,21 @@ package com.example.management;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
 
         EditText idText = (EditText) findViewById(R.id.idText);
         EditText passwordText = (EditText) findViewById(R.id.passwordText);
-        TextView welcomeMassage = (TextView) findViewById(R.id.welcomeMessage);
+        EditText nameText = (EditText) findViewById(R.id.nameText);
+        EditText ageText = (EditText) findViewById(R.id.ageText);
+
+        Button registerButton = (Button) findViewById(R.id.registerButton);
     }
 }
